fixed bad weighting sampler

data/video_segment_dataset.py
@@ -29,17 +29,17 @@ def compute_weights(ds,
                 if label==0:count_lk+=1
                 if label==1:count_llc+=1
                 if label==2:count_rlc+=1
-           class_counts = list((count_lk , count_rlc , count_llc))
+           class_counts = list((count_lk , count_llc , count_rlc))
            num_samples = sum(class_counts)
            class_weights = [num_samples/class_counts[i] for i in range(len(class_counts))]
            weights = [class_weights[labels[i]] for i in range(int(num_samples))]
            weights= torch.DoubleTensor(weights)
 
            if save:
-               torch.save(weights, "/home/iccs/Desktop/isense/events/intention_prediction/data/weights_torch/weights_union_prevention.pt")
+               torch.save(weights, "/home/iccs/Desktop/isense/events/intention_prediction/data/weights_torch/weights_union_prevention2.pt")
 
 
-           return weights
+           return weights , class_weights
 
 
 def _calculate_weigths_classes(maneuvers , lk):
@@ -56,21 +56,23 @@ def _calculate_weigths_classes(maneuvers , lk):
     w_llc=(_sum)/llc
     w_lk = (_sum)/lk
 
+    print("Weights lk,llc,rlc {} {} {}".format(w_lk ,w_llc,w_rlc))
     
     return {"LLC" : w_llc,
             "RLC": w_rlc,
             "LK": w_lk}
 
 def _segment_video(src="/home/iccs/Desktop/isense/events/intention_prediction/processed_data/video_camera1.mp4",
-                dstp = "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/segmented_frames/",**kwargs):
+                   dstp = "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/segmented_frames/",
+                   **kwargs):
     
     if kwargs["data_path"]:
         for i, src in enumerate(kwargs["data_path"]):
             src = src[1]
             i=i+2
-            dstp =  "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_0{}".format(i)
-            if not os.path.isdir(join(dstp,"segmented_frames")):os.mkdir(join(dstp,"segmented_frames"))
-            dstp = join(dstp,"segmented_frames")
+            dstp =  "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/"
+            if not os.path.isdir(join(dstp,"segmented_test_frames")):os.mkdir(join(dstp,"segmented_test_frames"))
+            dstp = join(dstp,"segmented_test_frames")
             print(src)
             cap = cv2.VideoCapture(src)
             frame_idx=0
@@ -86,7 +88,9 @@ def _segment_video(src="/home/iccs/Desktop/isense/events/intention_prediction/pr
                     else:
                         break
 
+        sys.exit()
 
+    
     cap = cv2.VideoCapture(src)
     frame_idx=0
     with tqdm(total=cap.get(cv2.CAP_PROP_FRAME_COUNT)) as pbar:
@@ -143,8 +147,8 @@ class prevention_dataset_train(Dataset):
                  root:str,
                  label_root:str) -> None:
         super().__init__()
-        self.H = 256
-        self.W = 256
+        self.H = 400
+        self.W = 400
         self.transform = Compose([ ToTensor() , Resize((self.H,self.W)) ]) #transfor for each read frame
 
         self.data=[]
@@ -178,50 +182,65 @@ class prevention_dataset_train(Dataset):
             self.data.append([frames_paths , maneuver_event])
             
 
+        if 1:
+            #assign lane keep clases
+            i=0
+            lk_counter=0
+            for maneuver in self.labels:
+                
+                frames_paths = sorted([join(root , str(x) + ".png") for x in range( i , maneuver[4])])
+                for j in range(len(frames_paths)//_PADDED_FRAMES):
+                    counter = 0
+                    _temp_list = []
+                    for frame in frames_paths:
+                        _temp_list.append(frame)
 
-        #assign lane keep clases
-        i=0
-        lk_counter=0
-        for maneuver in self.labels:
-            
-            frames_paths = sorted([join(root , str(x) + ".png") for x in range( i , maneuver[4])])
-            for j in range(len(frames_paths)//_PADDED_FRAMES):
-                counter = 0
-                _temp_list = []
-                for frame in frames_paths:
-                    _temp_list.append(frame)
+                        if counter==_PADDED_FRAMES:
+                            self.data.append([_temp_list , "LK"])
+                            lk_counter+=1
+                            break
+                        else:
+                            counter+=1
+                
+                i=maneuver[5] #assingn next start to end of current manuver
 
-                    if counter==_PADDED_FRAMES:
-                        self.data.append([_temp_list , "LK"])
-                        lk_counter+=1
-                        break
-                    else:
-                        counter+=1
-            
-            i=maneuver[5] #assingn next start to end of current manuver
-
-        # for j in range(0,20,self._MAX_VIDEO_FRAMES):
-        #     frames_temp = [] 
-        #     for frame_path in glob(join( root, "*.png")):
-        #         frames_temp.append(frame_path)
-        #         if len(frames_temp)==20:break
-                        
-        #     for maneuver_info in self.labels:
-        #         if frame_path in range(maneuver_info[5] - maneuver_info[4]):
-            
-        self.weights=_calculate_weigths_classes(maneuvers=self.labels, lk=lk_counter)
-
+            # for j in range(0,20,self._MAX_VIDEO_FRAMES):
+            #     frames_temp = [] 
+            #     for frame_path in glob(join( root, "*.png")):
+            #         frames_temp.append(frame_path)
+            #         if len(frames_temp)==20:break
+                            
+            #     for maneuver_info in self.labels:
+            #         if frame_path in range(maneuver_info[5] - maneuver_info[4]):
+                
+            self.weights=_calculate_weigths_classes(maneuvers=self.labels, lk=lk_counter)
+        self.lane_keep_counter,self.lane_change_right_counter,self.lane_change_left_counter=0,0,0
+        for item in self.data:
+            if item[1]=="LK":self.lane_keep_counter+=1
+            elif item[1]=="LLC":self.lane_change_right_counter+=1
+            elif item[1]=="RLC":self.lane_change_left_counter+=1
+            print("Currently have {} lane keep, {} left lane change , {} right lane change".format(self.lane_keep_counter,self.lane_change_left_counter,self.lane_change_right_counter))
         
-
 
 
     def __getitem__(self, index) -> Any:
             segment_paths , label = self.data[index]
             
             frame_stack = [cv2.imread(x) for x in segment_paths]
-            frame_tensor = torch.stack([self.transform(x) for x in frame_stack] , dim=1)
 
+            frame_tensor = torch.stack([self.transform(x) for x in frame_stack] , dim=1)
+            
+
+            # img=self.transform(cv2.imread(segment_paths[0])).cpu().permute((1,2,0)).numpy()
+           
+            # cv2.imwrite("/home/iccs/Desktop/isense/events/intention_prediction/example_pic2.png" , img)
+            # input("waiting")
+
+            # frame = cv2.imwrite(" /home/iccs/Desktop/isense/events/intention_prediction/example_pic.png",frame_tensor[:,:,:,].detach().cpu().numpy())
+            # input("waiting")
             assert  frame_tensor.size(0) == 3
+            assert frame_tensor.size(2) == self.H
+            assert frame_tensor.size(3) == self.W
 
             label_tensor = torch.tensor(self.class_map[label] , dtype = torch.long)
 
@@ -341,6 +360,116 @@ class prevention_dataset_val(Dataset):
         return len(self.data)
     
 
+class prevention_dataset_test(Dataset):
+    """
+    map style dataset:
+    Every _PADDED_FRAMES frames -> one prediction (Lane Keep)
+    Every delta frames (defined in lane_changes.txt) -> one pred (Lane change right/left)
+
+    return 4d rgb tensor
+
+    model always takes _PADDED frames and makes one prediction
+    """
+
+    def __init__(self,
+                 root,
+                 label_root,
+                 ) -> None:
+        super().__init__()
+        self.H = 245
+        self.W = 245
+        self.transform = Compose([ ToTensor() , Resize((self.H,self.W)) ]) #transfor for each read frame
+
+
+        self.data=[]
+        for video_frame_srcp in sorted(glob(join(root,".png"))):
+            self.data.append(video_frame_srcp)
+        
+        self.labels = _read_lane_change_labels(label_root)
+        self.class_map = {"LK":0, "LLC":1, "RLC":2}
+
+        #assign lane change clases
+
+        self.labels = self.labels[:]#train split 57 first maneuver-but need to assign last frame of previeous maneuver to set the index for lane keep in between data
+   
+
+        for maneuver_info in self.labels:
+            lane_start = maneuver_info[3]
+            lane_end = maneuver_info[4]
+            if lane_start<0:continue
+            maneuver_event = maneuver_info[2]
+            if maneuver_event!=3 and maneuver_event!=4:continue
+            assert maneuver_event==3 or maneuver_event==4, "Expected maneuver label in 3,4, got {}".format(maneuver_event)
+            # if lane_end-lane_start>72:continue  #skip maneuvers taking too long
+            if maneuver_event==4: maneuver_event="RLC"
+            if maneuver_event==3: maneuver_event="LLC"
+            
+            frames_paths = sorted([join(root , str(x) + ".png") for x in range(lane_start-5 , lane_end)])
+
+            assert len(frames_paths)>0,"The expected frame range is {} {}".format(lane_start-5 , lane_end)
+            
+            self.data.append([frames_paths , maneuver_event])
+
+
+        #assign lane keep clases
+        i=self.labels[0][5]
+        
+          #assign lane keep clases
+        lk_counter=0
+        for maneuver in self.labels:
+            
+            frames_paths = sorted([join(root , str(x) + ".png") for x in range( i , maneuver[4])])
+            
+            for j in range(len(frames_paths)//_PADDED_FRAMES):
+                counter = 0
+                _temp_list = []
+                for frame in frames_paths:
+                    _temp_list.append(frame)
+
+                    if counter==_PADDED_FRAMES:
+                        self.data.append([_temp_list , "LK"])
+                        lk_counter+=1
+                        break
+                    else:
+                        counter+=1
+                
+            
+            i=maneuver[5] #assingn next start to end of current manuver
+
+
+
+
+        # for j in range(0,20,self._MAX_VIDEO_FRAMES):
+        #     frames_temp = [] 
+        #     for frame_path in glob(join( root, "*.png")):
+        #         frames_temp.append(frame_path)
+        #         if len(frames_temp)==20:break
+                        
+        #     for maneuver_info in self.labels:
+        #         if frame_path in range(maneuver_info[5] - maneuver_info[4]):
+
+
+    def __getitem__(self, index) -> Any:
+            segment_paths , label = self.data[index]
+
+            img = cv2.imread(segment_paths[0])
+            
+            frame_stack = [cv2.imread(x) for x in segment_paths]
+            frame_tensor = torch.stack([self.transform(x) for x in frame_stack] , dim=1)
+
+            frame = cv2.imwrite(" /home/iccs/Desktop/isense/events/intention_prediction/example_pic.png",frame_tensor[:,:,:,].detach().cpu().numpy())
+            # cv2.imwrite(" /home/iccs/Desktop/isense/events/intention_prediction/example_pic.png",frame )
+
+            # assert frame_tensor.size(1) == _PADDED_FRAMES and frame_tensor.size(0) == 3
+
+            label_tensor = torch.tensor(self.class_map[label] , dtype = torch.float)
+
+            return frame_tensor , label_tensor
+        
+        
+    def __len__(self):
+        return len(self.data)
+
 def construct_ds():
     """
     construct concat dataset from concatenation using torch.util.data.ConcatDataset
@@ -363,10 +492,12 @@ class union_prevention(prevention_dataset_train , Dataset):
 
 
 if __name__=="__main__":
-    data_labels_path = [
-                        # ("/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_02", "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_02/video_camera1.mp4") , 
-                        ("/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_03", "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_03/video_camera1.mp4"),
-                        ("/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_04", "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_04/video_camera1.mp4"),
-                        # ("/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_05", "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_05/'video_camera1(4).mp4'")
-                        ]
+    # data_labels_path = [
+    #                     # ("/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_02", "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_02/video_camera1.mp4") , 
+    #                     ("/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_03", "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_03/video_camera1.mp4"),
+    #                     ("/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_04", "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_04/video_camera1.mp4"),
+    #                     # ("/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_05", "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_05/'video_camera1(4).mp4'")
+    #                     ]
+    data_labels_path = [("/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/recording_04/drive_03/processed_data/",
+                        "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/recording_04/drive_03/video_camera1.mp4")]
     _segment_video(data_path = data_labels_path)

my_app.py
@@ -12,9 +12,11 @@ from torch.utils.data import ChainDataset,ConcatDataset
 from intention_prediction.data.prevention_data_iter import *
 from intention_prediction.data.preprocess_labels import _preprocess_label_file
 from intention_prediction.src.train_epochs import *
+from intention_prediction.src.test import *
 from intention_prediction.models.load_resnet import *
 from intention_prediction.data.data_loader_utils import collate_fn_padding
-from intention_prediction.data.video_segment_dataset import prevention_dataset_val , prevention_dataset_train, construct_ds, compute_weights
+from intention_prediction.data.video_segment_dataset import (prevention_dataset_val , prevention_dataset_train, 
+                                                               construct_ds, compute_weights , prevention_dataset_test)
 
 from itertools import cycle
 
@@ -56,19 +58,21 @@ def my_app(cfg: DictConfig):
 
     dataset_train = ConcatDataset([prevention_dataset_train(root= "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/segmented_frames",
                                              label_root="/home/iccs/Desktop/isense/events/intention_prediction/processed_data/detection_camera1/lane_changes.txt"),
-                                prevention_dataset_train(root= "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_02/segmented_frames",
-                                             label_root="/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_02/processed_data/detection_camera1/lane_changes.txt"),
-                                prevention_dataset_train(root= "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_03/segmented_frames",
-                                             label_root="/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_03/processed_data/detection_camera1/lane_changes.txt")
-                                ])
+                                    prevention_dataset_train(root= "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_02/segmented_frames",
+                                                label_root="/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_02/processed_data/detection_camera1/lane_changes.txt"),
+                                    prevention_dataset_train(root= "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_03/segmented_frames",
+                                                label_root="/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/processed_data_03/processed_data/detection_camera1/lane_changes.txt")
+                                    ])
     
+    dataset_test = prevention_dataset_test(root= "/home/iccs/Desktop/isense/events/intention_prediction/processed_data/segmented_test_frames",
+                                           label_root="/home/iccs/Desktop/isense/events/intention_prediction/processed_data/new_data/recording_04/drive_03/processed_data/detection_camera1/lane_changes.txt")
+
     
-    
-    if os.path.isfile(s:="/home/iccs/Desktop/isense/events/intention_prediction/data/weights_torch/weights_union_prevention.pt"):
+    if os.path.isfile(s:="/home/iccs/Desktop/isense/events/intention_prediction/data/weights_torch/weights_union_prevention2.pt"):
         weights=torch.load(s)
         print(weights)
-    else: weights = compute_weights(ds = dataset_train,)
-
+    else: weights , class_w = compute_weights(ds = dataset_train,)
+    input(class_w)
     print("Final dataset size is {}".format(len(dataset_train)))
 
     # dataloader = instantiate(config = cfg.datasets.prevention_loader)  #recheck
@@ -78,7 +82,9 @@ def my_app(cfg: DictConfig):
                                   pin_memory=True)
     
     dataloader_val = DataLoader(dataset_val , batch_size=1 , collate_fn= collate_fn_padding , shuffle=True)
-    # dataloader_test = DataLoader(dataset_test , batch_size=1 , collate_fn= collate_fn_padding)
+
+
+    dataloader_test = DataLoader(dataset_test , batch_size=1 , collate_fn= collate_fn_padding)
 
     # model = instantiate(cfg.conf.models)   #recheck 
     model = torch.hub.load('facebookresearch/pytorchvideo', 'slow_r50', pretrained=False , model_num_class=3)
@@ -100,26 +106,61 @@ def my_app(cfg: DictConfig):
 
     writer = SummaryWriter(log_dir= "/home/iccs/Desktop/isense/events/intention_prediction/logs/run_{}".format(now:=datetime.datetime.now()) )
     
-        
-
-
-    train(cfg , writer=writer,
-          dataloader_train = dataloader_train , 
-          dataloader_val = dataloader_val,
-          dataset_train = dataset_train,
-          dataset_val = dataset_val,
-          weights = weights ,
-          model = model , 
-          optimizer = optimizer,
-          scheduler = scheduler,
-          epochs = epochs,
-          dev= dev,
-          model_save_path="/home/iccs/Desktop/isense/events/intention_prediction/models/weights/train_01_03_04.pt",
-          num_iterations_gr_accum = 16,
-          log_dict = {"lr":0.003},
-          load_saved_model ="/home/iccs/Desktop/isense/events/intention_prediction/models/weights/train_01_03_04.pt")
     
-    # test(cfg , dataloader_test , model , dev)
+    if 1:
+        train(cfg , writer=writer,
+            dataloader_train = dataloader_train , 
+            dataloader_val = dataloader_val,
+            dataset_train = dataset_train,
+            dataset_val = dataset_val,
+            weights = weights ,
+            model = model , 
+            optimizer = optimizer,
+            scheduler = scheduler,
+            epochs = epochs,
+            dev= dev,
+            model_save_path="/home/iccs/Desktop/isense/events/intention_prediction/models/weights/train_01_03_04higher_res.pt",
+            num_iterations_gr_accum = 16,
+            log_dict = {"lr":0.003},
+            load_saved_model ="/home/iccs/Desktop/isense/events/intention_prediction/models/weights/train_01_03_04.pt")
+    
+    test(cfg , 
+        writer=writer,
+        dataloader_test= dataloader_test,
+        weights = weights ,
+        model = model , 
+        epochs = epochs,
+        dev= dev,
+        load_saved_model ="/home/iccs/Desktop/isense/events/intention_prediction/models/weights/train_01_03_04_higher_res.pt")
+
+@torch.no_grad()
+def construct_inference_video(
+                              model,
+                              srcp="/home/iccs/Desktop/isense/events/intention_prediction/processed_data/video_test.avi",
+                              dstp="/home/iccs/Desktop/isense/events/intention_prediction/outputs/output.avi",
+                              ):
+    cap_in =cv2.VideoCapture(srcp)
+    total_frames = cap_in.get(cv2.CAP_PROP_MAX_FRAMES)
+
+    cap_in.set(cv2.CAP_PROP_POS_FRAMES , total_frames*0.9)
+
+    fourcc = cv2.VideoWriter_fourcc(*"MPEG")
+    cap = cv2.VideoWriter(dstp , fourcc,fps,(H,W))
+
+    with tqdm(total=0.1*total_frames):
+        frame_list = []
+        for idx in range(0,prediction_window:=30):
+            ret,frame=cap_in.read()
+            frame_list.append(frame)
+        
+        frame_tensor = inference_transform(frame)
+        predictions = model(frame_list)
+        img = frame_tensor.detach().cpu().numpy()
+        
+        img = cv2.writeText(img , "online prediction is {}".format(np.argmax(predictions)))
+
+        cap.write(img)
+
 
 if __name__=="__main__":
     """

src/test.py
@@ -1,5 +1,5 @@
 from typing import Dict
-from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score,balanced_accuracy_score,f1_score
 import torch
 from torchvision.utils import np
 from tqdm import tqdm
@@ -10,46 +10,98 @@ def test(*args , **kwargs)->Dict:
         """
         test
         """
-        data_loader = kwargs["dataloader_val"]
+        writer = kwargs["writer"]
+        val_losses_dict = test_batches(**kwargs)
+        epoch=0
+        for i, (desc , val) in enumerate(val_losses_dict.items()):
+          
+            if desc=="val_pres":
+                for i in range(2):
+                    writer.add_scalars("Val micro (0=Lk,1=Llc,2=Rlc)" , {"Class {}".format(i):val[1]},  i)   # add epoch wise acc,pres etc metrics 
+                writer.add_scalar("Val Macro" , val[0],  epoch)   # add epoch wise acc,pres etc metrics 
+
+                continue
+            if desc =="val_pres_weighted":
+                for i in range(2):
+                    print(val)
+                    writer.add_scalars("val_pres_weighted" , {"Class {}".format(i):val[1]},  i)   # add epoch wise acc,pres etc metrics 
+                continue
+            if desc=="val_rec":
+                for i in range(2):
+                    writer.add_scalars("Rec micro" , {"Class {}".format(i):val[1]},  i)   # add epoch wise acc,pres etc metrics 
+
+                writer.add_scalar("Rec macro" , val[0],  epoch)   # add epoch wise acc,pres etc metrics 
+                continue
+        
+            if desc=="acc":
+                writer.add_scalar(desc , val,  epoch)   # add epoch wise acc,pres etc metrics 
+                              
+
+def test_batches(*args , **kwargs):
+        data_loader = kwargs["dataloader_test"]
         dev=kwargs["dev"]
         model = kwargs["model"]
-        criterion = kwargs["criterion"]
-        writer = kwargs["writer"]
-
+        
         loss_epoch = []
         predictions_epoch = []
         labels_epoch = []
         max_epochs_val = 0
 
-        for batch_idx , (frames , maneuver_type) in (pbar:=tqdm(enumerate(data_loader))): 
+        print("Loading saved model at path {}".format(kwargs["load_saved_model"]))
+        checkpoint = torch.load(kwargs["load_saved_model"])
+        model.load_state_dict(checkpoint["model_state_dict"])
+        model=model.to(dev)
 
-            pbar.set_description_str("Batch: {}/{}".format(batch_idx))
+        for batch_idx , (frames , maneuver_type) in (pbar:=tqdm(enumerate(data_loader))): 
+            pbar.set_description_str("Val Batch: {}".format(batch_idx))
             
-            frames = frames.to(dev)
-            maneuver_type=maneuver_type.to(dev)
+            frames = frames.to(dev) 
+            maneuver_type=maneuver_type.type(torch.LongTensor).to(dev)
+            
+
             prediction = model(frames)
-            prediction =torch.nn.Softmax(prediction)
            
-            loss = criterion(prediction , maneuver_type)
 
             #to comute eval metrics
             predictions_epoch.append(prediction.detach().cpu().numpy())
             labels_epoch.append(maneuver_type.detach().cpu().numpy())
 
-            loss_epoch.append(loss.item())
+            last_predictions_epoch=list(map(lambda x: np.argmax(x) , predictions_epoch))
+            last_labels_epoch=list(map(lambda x: int(x) , labels_epoch))
 
-            pbar.set_postfix_str("Val Batch loss {:0.2f}".format(loss.item()))
+            print(last_predictions_epoch)
+            print(last_labels_epoch)
+            pbar.set_postfix_str("Running Accuracy {:0.2f}".format(accuracy_score(last_labels_epoch , last_predictions_epoch)))
+            print("Running Accuracy {:0.2f}".format(accuracy_score(last_labels_epoch , last_predictions_epoch)))
+            print("Running Pr {:0.2f}".format(precision_score(last_labels_epoch , last_predictions_epoch,average="weighted")))
+            print("Running Bacc {:0.2f}".format(balanced_accuracy_score(last_labels_epoch , last_predictions_epoch)))
+            print("Running F1 {:0.2f}".format(f1_score(last_labels_epoch , last_predictions_epoch,average="weighted")))
 
 
+
+            max_epochs_val+=1
+
+        #convert to int categorical labels
+        predictions_epoch=list(map(lambda x: np.argmax(x) , predictions_epoch))
+        labels_epoch=list(map(lambda x: int(x) , labels_epoch))
+
+        
         acc = accuracy_score(labels_epoch , predictions_epoch)
-        pres=precision_score(labels_epoch , predictions_epoch)
-        rec =recall_score(labels_epoch , predictions_epoch)
+        pres_avg=precision_score(labels_epoch , predictions_epoch , average = "macro")
+        pres_class=precision_score(labels_epoch , predictions_epoch , average = "micro")
+        pres=precision_score(labels_epoch , predictions_epoch , average = "weighted")
 
-        return {
+        rec =recall_score(labels_epoch , predictions_epoch , average="macro")
+        rec_class = recall_score(labels_epoch , predictions_epoch , average= "micro")
+
+
+        print(pres_class)
+        return {"loss_val_epoch":np.array(loss_epoch),
                 "val_acc":acc,
-                "val_pres":pres , 
-                "val_rec":rec ,
-                "batch_count":max_epochs_val}
+                "val_pres":[pres_avg , pres_class] , 
+                "val_rec":[rec,rec_class] ,
+                "batch_count":max_epochs_val , 
+                "val_pres_global":pres}
 
 def test_conf(*args , **kwargs):
       writer = kwargs["writer"]

src/train_epochs.py
@@ -7,7 +7,7 @@ from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from sklearn.metrics import accuracy_score, precision_score,recall_score
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, precision_score,recall_score
 from ..data.data_loader_utils import collate_fn_padding
 
 from ..data.prevention_data_iter import read_frame_from_iter_train, read_frame_from_iter_val
@@ -103,7 +103,7 @@ def train(*args,**kwargs):
         torch.save({'epoch': epoch,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
-            "loss" : losses_dict["loss_train_epoch"][-1],
+            "loss" : losses_dict["desc"][-1],
             }, kwargs["model_save_path"])
 
         val_losses_dict = val_one_epoch(*args, **kwargs)  #vla losses dict
@@ -115,11 +115,18 @@ def train(*args,**kwargs):
                  continue
             if desc=="val_pres":
                 for i in range(2):
-                    writer.add_scalars("Val micro (0=Lk,1=Llc,2=Rlc)" , {"Class {}".format(class_map[i]):val[1][i]},  i)   # add epoch wise acc,pres etc metrics 
+                    writer.add_scalars("Val micro (0=Lk,1=Llc,2=Rlc)" , {"Class {}".format(i):val[1]},  i)   # add epoch wise acc,pres etc metrics 
+                writer.add_scalar("Val Macro" , val[0],  epoch)   # add epoch wise acc,pres etc metrics 
+
+                continue
+            if desc =="val_pres_weighted":
+                for i in range(2):
+                    print(val)
+                    writer.add_scalars("val_pres_weighted" , {"Class {}".format(i):val[1]},  i)   # add epoch wise acc,pres etc metrics 
                 continue
             if desc=="val_rec":
                 for i in range(2):
-                    writer.add_scalars("Rec micro" , {"Class {}".format(class_map[i]):val[1][i]},  i)   # add epoch wise acc,pres etc metrics 
+                    writer.add_scalars("Rec micro" , {"Class {}".format(class_map[i]):val[1]},  i)   # add epoch wise acc,pres etc metrics 
 
                 writer.add_scalar("Rec macro" , val[0],  epoch)   # add epoch wise acc,pres etc metrics 
                 continue
@@ -131,7 +138,6 @@ def train(*args,**kwargs):
 
 
 
-            writer.add_scalar("Val Macro" , val[0],  epoch)   # add epoch wise acc,pres etc metrics 
 
 
         scheduler.step()
@@ -180,8 +186,6 @@ def train_one_epoch(*args , **kwargs):
         predictions_epoch=[]
         labels_epoch=[]
 
-       
-
         for batch_idx , (frames , maneuver_type) in (pbar:=tqdm(enumerate(data_loader))): 
 
             pbar.set_description_str("Train Batch: {}".format(batch_idx))
@@ -204,14 +208,14 @@ def train_one_epoch(*args , **kwargs):
                 max_batches+=1
                 writer.add_scalar("Online batch loss-During update ", loss_epoch[-1] , batch_idx)
 
+
             pbar.set_postfix_str("Batch loss {:0.2f}".format(loss.item()))
 
             predictions_epoch.append(prediction.detach().cpu().numpy())
             labels_epoch.append(maneuver_type.detach().cpu().numpy())
 
-            
-        
-        acc = accuracy_score(labels_epoch , predictions_epoch )      
+
+        acc = accuracy_score(labels_epoch , s:=list(map(lambda x:np.argmax(x) , predictions_epoch))  )  
 
         return {"desc":"loss_train_epoch",
                 "val":np.array(loss_epoch),
@@ -250,7 +254,7 @@ def val_one_epoch(*args , **kwargs)->Dict:
             labels_epoch.append(maneuver_type.detach().cpu().numpy())
 
             loss_epoch.append(loss.item())
-
+       
             pbar.set_postfix_str("Val Batch loss {:0.2f}".format(loss.item()))
 
             max_epochs_val+=1
@@ -261,17 +265,21 @@ def val_one_epoch(*args , **kwargs)->Dict:
 
         
         acc = accuracy_score(labels_epoch , predictions_epoch)
+        bacc = balanced_accuracy_score(labels_epoch , predictions_epoch)
+
         pres_avg=precision_score(labels_epoch , predictions_epoch , average = "macro")
         pres_class=precision_score(labels_epoch , predictions_epoch , average = "micro")
-        # pres_class=precision_score(labels_epoch , predictions_epoch , average = None)
+        pres=precision_score(labels_epoch , predictions_epoch , average = "weighted")
 
-        
         rec =recall_score(labels_epoch , predictions_epoch , average="macro")
         rec_class = recall_score(labels_epoch , predictions_epoch , average= "micro")
+
 
         print(pres_class)
         return {"loss_val_epoch":np.array(loss_epoch),
                 "val_acc":acc,
                 "val_pres":[pres_avg , pres_class] , 
                 "val_rec":[rec,rec_class] ,
-                "batch_count":max_epochs_val}
+                "batch_count":max_epochs_val , 
+                "val_pres_global":pres,
+                "bacc":bacc}